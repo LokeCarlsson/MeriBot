bc85a7b2fdf32286bf06c11c0c141f0cb8cc014c
LokeCarlsson <lc222ak@student.lnu.se>
2016-12-09 21:51:15 +0100

    fix

--- main.js
+++ main.js
@@ -19,51 +19,6 @@ class ClientWrapper {
 		this.core.json(this.service, clientConfig, callback)
 	}
 
-<<<<<<< HEAD
-var core = new ImageVault.Client({
-	core: "http://iv5qa.azurewebsites.net/apiv2",
-	username: "hackathon",
-	password: "ImageVault2016"
-});
-
-
-core.json("MediaService/Find", {
-  MediaUrlBase: "http://iv5qa.azurewebsites.net/",
-  Populate: {
-	PublishIdentifier: "hackathon",
-	MediaFormats: [{
-		$type : "ImageVault.Common.Data.ThumbnailFormat,ImageVault.Common",
-		Effects : [{
-			$type : "ImageVault.Common.Data.Effects.ResizeEffect,ImageVault.Common",
-			"Width" : 200,
-			"Height" : 200,
-			"ResizeMode" : "ScaleToFill"
-		  }],
-	  }
-	],
-	Metadata: [{
-		Filter: {
-			MetadataDefinitionType : "User"
-		}
-	}]
-},
-  "Filter" : {
-	"SearchString" : "alche"
-  }
-}, function (d) {
-	console.log(d);
-	if (d == null || d.length == null) {
-	  console.log("Nothing found!");
-	} else {
-	  console.log("Found " + d.length + " hits.");
-	}
-
-	for (var i = 0; i < d.length; i++) {
-		var item = d[i];
-		console.log("item :"+i+item);
-		var thumbnail = item.MediaConversions[0];
-		console.log("<div style='float:left;text-align:center;'><img src='" + thumbnail.Url + "'/><br/>"+item.Name+"</div>");
-=======
 	clientRequestConfig(searchString) {
 		return {
 		  MediaUrlBase: "http://iv5qa.azurewebsites.net/",
@@ -86,10 +41,9 @@ core.json("MediaService/Find", {
 			}]
 		},
 		  "Filter" : {
-			"SearchString" : searchString 
+			"SearchString" : searchString
 		  }
 		}
->>>>>>> gren
 	}
 }
 
