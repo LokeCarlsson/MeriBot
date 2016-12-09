e88a809f1bec60fe1d8d5e11ef15585801914a03
LokeCarlsson <lc222ak@student.lnu.se>
2016-12-09 19:51:44 +0100

    Added name check and beautified the file

--- bot.js
+++ bot.js
@@ -9,11 +9,50 @@ const controller = Botkit.slackbot({
 
 const welcomeMessage = "Hi, I'm MeriBot!\nMention me and your request, about the following:\n• *pictures*\n• *videos*";
 
-
 // connect the bot to a stream of messages
 controller.spawn({
   token: process.argv[2],
 }).startRTM()
+
+
+controller.hears(['help'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+  bot.reply(message, welcomeMessage);
+});
+
+controller.hears(['cat'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+  bot.reply(message, {
+    "attachments": [{
+      "fallback": "This is supposed to be a silly cat",
+      "title": "Silly cat",
+      "title_link": "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg",
+      "text": "Here you have a picture of a silly cat",
+      "image_url": "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg",
+      "color": "#B4DA55"
+    }]
+  });
+});
+
+controller.hears('pictures', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+  bot.startConversation(message, function(err, convo) {
+    convo.ask('What do you want to show picture of?', function(response, convo) {
+      convo.say('Alright, showing pictures of: ' + response.text);
+      convo.next();
+    });
+  })
+});
+
+controller.hears('videos', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+  bot.startConversation(message, function(err, convo) {
+    convo.ask('What do you want to show videos of?', function(response, convo) {
+      convo.say('Alright, showing videos of: ' + response.text);
+      convo.next();
+    });
+  })
+});
+
+/*
+ * Trivial and silly stuff!
+ */
 
 controller.hears(['hello', 'hi', 'hey'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
   console.log("Hej");
@@ -32,7 +71,7 @@ controller.hears(['hello', 'hi', 'hey'], ['direct_message', 'direct_mention', 'm
   });
 });
 
-controller.hears(['good', 'perfect', 'awesome', 'sweet'],['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+controller.hears(['good', 'perfect', 'awesome', 'sweet', 'nice'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
   bot.api.reactions.add({
     timestamp: message.ts,
     channel: message.channel,
@@ -40,7 +79,7 @@ controller.hears(['good', 'perfect', 'awesome', 'sweet'],['direct_message', 'dir
   });
 });
 
-controller.hears(['call me (.*)', 'my name is (.*)'],['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+controller.hears(['call me (.*)', 'my name is (.*)'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
   var name = message.match[1];
   controller.storage.users.get(message.user, function(err, user) {
     if (!user) {
@@ -55,37 +94,57 @@ controller.hears(['call me (.*)', 'my name is (.*)'],['direct_message', 'direct_
   });
 });
 
-controller.hears(['help'],['direct_message','direct_mention','mention'], function(bot,message) {
-  bot.reply(message, welcomeMessage);
-});
+controller.hears(['what is my name', 'who am i'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+  controller.storage.users.get(message.user, function(err, user) {
+    if (user && user.name) {
+      bot.reply(message, 'Your name is ' + user.name);
+    } else {
+      bot.startConversation(message, function(err, convo) {
+        if (!err) {
+          convo.say('I do not know your name yet!');
+          convo.ask('What should I call you?', function(response, convo) {
+            convo.ask('You want me to call you `' + response.text + '`?', [{
+              pattern: 'yes',
+              callback: function(response, convo) {
+                convo.next();
+              }
+            }, {
+              pattern: 'no',
+              callback: function(response, convo) {
+                convo.stop();
+              }
+            }, {
+              default: true,
+              callback: function(response, convo) {
+                convo.repeat();
+                convo.next();
+              }
+            }]);
+            convo.next();
+          }, {
+            'key': 'nickname'
+          });
 
-controller.hears(['cat'], ['direct_message', 'direct_mention', 'mention'], function(bot,message){
-  bot.reply(message, {
-    "attachments": [{
-      "fallback": "This is supposed to be a silly cat",
-      "title": "Silly cat",
-      "title_link": "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg",
-      "text": "Here you have a picture of a silly cat",
-      "image_url": "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg",
-      "color": "#B4DA55"
-    }]
+          convo.on('end', function(convo) {
+            if (convo.status == 'completed') {
+              bot.reply(message, 'OK! I will update my dossier...');
+              controller.storage.users.get(message.user, function(err, user) {
+                if (!user) {
+                  user = {
+                    id: message.user,
+                  };
+                }
+                user.name = convo.extractResponse('nickname');
+                controller.storage.users.save(user, function(err, id) {
+                  bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                });
+              });
+            } else {
+              bot.reply(message, 'OK, nevermind!');
+            }
+          });
+        }
+      });
+    }
   });
-});
-
-controller.hears('pictures',['direct_message','direct_mention','mention'],function(bot,message) {
-  bot.startConversation(message,function(err,convo) {
-    convo.ask('What do you want to show picture of?',function(response,convo) {
-      convo.say('Alright, showing pictures of: ' + response.text);
-      convo.next();
-    });
-  })
-});
-
-controller.hears('videos',['direct_message','direct_mention','mention'],function(bot,message) {
-  bot.startConversation(message,function(err,convo) {
-    convo.ask('What do you want to show videos of?',function(response,convo) {
-      convo.say('Alright, showing videos of: ' + response.text);
-      convo.next();
-    });
-  })
 });
